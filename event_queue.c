0c04cb8c79520ed4b180f2393cc623180808ff20
koonwen <koonwen@gmail.com>
2022-02-22 18:24:25 +0100

    Work on event_queue

--- event_queue.c
+++ event_queue.c
@@ -1,13 +1,20 @@
 #include "event_queue.h"
 
-void myhandler(void *arg)
-{
-    printf("\nUART event called\n");
-}
+void savebyte(void *arg);
+uint8_t BYTE_CONTAINER;
 
+// Install interrupt logic upon UART event
 void uart_cb(void *arg, uint8_t data)
 {
+    printf("ISR: (unsigned int) %u\n\r", data);
     event_t *event = (event_t *)calloc(sizeof(1), sizeof(event_t));
-    event->handler = myhandler;
+    event_callback_init(event, savebyte, data);
     event_post(&QUEUE, event);
+}
+
+// Callback fired on interrupt event
+void savebyte(void *arg)
+{
+    BYTE_CONTAINER = (uint8_t)arg;
+    printf("UART EVENT CALLED, got %c\n\r", BYTE_CONTAINER);
 }
--- event_queue.h
+++ event_queue.h
@@ -1,11 +1,16 @@
 #ifndef EVENT_QUEUE_H
 #define EVENT_QUEUE_H
 
+#include "string.h"
+
 #include "xtimer.h"
 #include "event.h"
 #include "periph/uart.h"
+#include "event/callback.h"
+#include "stdio_uart.h"
 
 extern event_queue_t QUEUE;
+extern uint8_t BYTE_CONTAINER;
 
 void uart_cb(void *arg, uint8_t data);
 
